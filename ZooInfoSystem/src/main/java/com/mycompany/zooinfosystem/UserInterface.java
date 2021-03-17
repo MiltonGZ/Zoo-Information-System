e83d142b5d385cbf41d84fac07dc4a0e50fc8017
MiltonGZ <64173792+MiltonGZ@users.noreply.github.com>
2021-03-17 17:32:23 -0400

    last commit for assignment Homework 3

--- ZooInfoSystem/src/main/java/com/mycompany/zooinfosystem/UserInterface.java
+++ ZooInfoSystem/src/main/java/com/mycompany/zooinfosystem/UserInterface.java
@@ -12,8 +12,10 @@ package com.mycompany.zooinfosystem;
 import java.util.Scanner;
 public class UserInterface {
     private Scanner scanner;
+    private int empOrCust = 0;
     public UserInterface(Scanner scanner){
         this.scanner = scanner;
+        this.empOrCust = empOrCust;
     }
     
     public void start(){
@@ -21,30 +23,52 @@ public class UserInterface {
             System.out.println("Are you an employee or customer?");
             System.out.println(" 1 - Employee");
             System.out.println(" 2 - Customer");
-            System.out.println(" 3 - stop");
-            int empOrCust = Integer.valueOf(scanner.nextLine());
-            switch (empOrCust) {
-                case 1:
-                    if(empOrCust==1){
-                    }
-                case 2:
-                    if(empOrCust==2){
-                    }
-                case 3:
-                    if(empOrCust==3){
-                    }
-                default:
-                    break;
+            System.out.println(" 3 - Log out");
+            //option 4 is a hidden option for managers
+            this.empOrCust = Integer.valueOf(scanner.nextLine());
+            if(empOrCust==3){
+                System.out.println("Come back again!");
+                break;
+            }else{
+                firstMenu();
             }
+            
             
             
         }
     }
+    public void firstMenu(){
+        switch (empOrCust) {
+                case 1:
+                    if(empOrCust==1){
+                        empMenu();
+                    }
+                case 2:
+                    if(empOrCust==2){
+                        customerMenu();
+                    }
+                case 3:
+                    if(empOrCust==4){
+                        int keyCode = 2021;
+                        System.out.println("Enter keycode");
+                        int prompt = Integer.valueOf(scanner.nextLine());
+                        if(prompt==keyCode){
+                            managerMenu();
+                        }else{
+                            System.out.println("You're not a Manager!");
+                            break;
+                        }
+                    }
+            }
+    }
     public void customerMenu(){
-        
+        System.out.println("What would you like to do?");
     }
     public void empMenu(){
-        
+        System.out.println("What would you like to do?");
+    }
+    public void managerMenu(){
+        System.out.println("What would you like to do?");
     }
     
 }
